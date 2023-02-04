b775219902e15c92c991597c41cd9f730bf5f9ac
Leeroy Liu <leeroyliu999@gmail.com>
2023-02-04 19:37:31 +0100

    Add small change to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,14 +8,7 @@ public class GameManager : MonoBehaviour
     public static int recipesBrewed = 0;
     private static int recipesToWin = 5;
     private int timeToLose = 60 * 2; // 2 mins
-    public static Dictionary<Constants.Ingredient, int> inventory = new Dictionary<Constants.Ingredient, int>{
-        {Constants.Ingredient.Root1, 0},
-        {Constants.Ingredient.Root2, 0},
-        {Constants.Ingredient.Root3, 0},
-        {Constants.Ingredient.Item1, 0},
-        {Constants.Ingredient.Item2, 0},
-        {Constants.Ingredient.Item3, 0},
-    };
+    public static Dictionary<Constants.Ingredient, int> inventory;
 
     private static Dictionary<Constants.Ingredient, int> answerPotion;
 
@@ -39,7 +32,7 @@ public class GameManager : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        
+        ResetInventory();
         answerPotion = Interaction.generateAnswer(inventory);
     }
 
@@ -50,12 +43,14 @@ public class GameManager : MonoBehaviour
 
     private static void ResetInventory()
     {
-        inventory[Constants.Ingredient.Root1] = 0;
-        inventory[Constants.Ingredient.Root2] = 0;
-        inventory[Constants.Ingredient.Root3] = 0;
-        inventory[Constants.Ingredient.Item1] = 0;
-        inventory[Constants.Ingredient.Item2] = 0;
-        inventory[Constants.Ingredient.Item3] = 0;
+        inventory = new Dictionary<Constants.Ingredient, int>{
+            {Constants.Ingredient.Root1, 0},
+            {Constants.Ingredient.Root2, 0},
+            {Constants.Ingredient.Root3, 0},
+            {Constants.Ingredient.Item1, 0},
+            {Constants.Ingredient.Item2, 0},
+            {Constants.Ingredient.Item3, 0},
+        };
     }
 
     public static bool BrewMixture()
