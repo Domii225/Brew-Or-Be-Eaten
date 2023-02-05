ebf43fb184a2a77dfdecbcee1100b95696cc2aad
Domi_1 <dominika.dlugowolska@3dinteractive.se>
2023-02-05 16:45:01 +0100

    b

--- Assets/Scripts/FeedMonster.cs
+++ Assets/Scripts/FeedMonster.cs
@@ -19,38 +19,35 @@ public class FeedMonster : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
-        Debug.Log(other.tag);
-
-        if (other.CompareTag("Human"))
+        Transform t = other.transform;
+        while (t.parent != null)
         {
-            
-            if (other.name.Contains("Success"))
-            {
-                GameManager.recipesBrewed++;
-                if (GameManager.recipesBrewed == 1)
-                {
-                    GameManager.StartTimer();
-                    GameManager.answerPotion = Interaction.generateAnswer(GameManager.inventory);
-                    GameManager.gameState = Constants.GameState.InGame;
-                    Debug.Log("YOUR TIMER STARTS NOW"); // Monster growl
-                    GameManager.MonsterMessage("YOUR TIMER STARTS NOW");
-                }
-                if (GameManager.recipesBrewed >= GameManager.recipesToWin)
-                {
-                    GameManager.gameState = Constants.GameState.WinGame;
-                }
-            }
-            else
-            {
-                GameManager.tries++;
-                Shake.shakeAmount = GameManager.tries * shakeStrength;
-                Shake.shakeStatic = true;
-                Debug.Log(other.name); // Monster growl
-                GameManager.MonsterMessage(other.name);
-            }
-            Destroy(other.gameObject);
+            t = t.parent.gameObject.transform;
         }
-        
+        if (t.name.Contains("Success"))
+        {
+            GameManager.recipesBrewed++;
+            if (GameManager.recipesBrewed == 1)
+            {
+                GameManager.StartTimer();
+                GameManager.answerPotion = Interaction.generateAnswer(GameManager.inventory);
+                GameManager.gameState = Constants.GameState.InGame;
+                Debug.Log("YOUR TIMER STARTS NOW"); // Monster growl
+                GameManager.MonsterMessage("YOUR TIMER STARTS NOW");
+            }
+            if (GameManager.recipesBrewed >= GameManager.recipesToWin)
+            {
+                GameManager.gameState = Constants.GameState.WinGame;
+            }
+        }
+        else
+        {
+            GameManager.tries++;
+            Shake.shakeAmount = GameManager.tries * shakeStrength;
+            Shake.shakeStatic = true;
+            Debug.Log(t.name); // Monster growl
+            GameManager.MonsterMessage(t.name);
+        }
+        Destroy(t.gameObject);
     }
 }
